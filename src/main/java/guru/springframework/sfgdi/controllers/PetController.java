88cf2f666c6d733510c5da25f4b75c79c46d4f61
Wafa MLIKA <wafa.mlika@vermeg.com>
2022-08-19 15:37:08 +0100

    Ex-4

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,13 +1,19 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Component
 public class PetController {
 
     private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
 
     public String whichPetIsTheBest(){
         return petService.getPetType();
